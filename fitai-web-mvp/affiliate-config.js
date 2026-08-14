f1ab70ce73c46ab40f16f80a1092208d9192f107
yahyamohd173-web <yahyamohd173@gmail.com>
2026-08-14 23:48:14 +0530

    Add affiliate-ready retailer configuration and disclosure

---
+++ fitai-web-mvp/affiliate-config.js
@@ -0,0 +1,8 @@
+window.FITAI_AFFILIATE={
+  amazon:{tag:"",enabled:false},
+  flipkart:{affid:"",enabled:false},
+  myntra:{partnerId:"",enabled:false},
+  ajio:{partnerId:"",enabled:false},
+  tataCliq:{partnerId:"",enabled:false},
+  nykaaFashion:{partnerId:"",enabled:false}
+};
